2c65ccaed53896205f985983f44ca69805cb17ee
kameiryousuke <kamei.r@i.softbank.jp>
2022-08-27 02:04:10 +0900

    types = 型定義の完了

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import { GetServerSideProps, NextPage } from "next";
+import React, { FC } from "react";
 import ArticleMeta from "../../components/ArticleMeta";
 import Layout from "../../components/Layout";
+import { ArticleProps, pageProps, params } from "../../types/type";
+import { sampleCards } from "../../utils/sample";
 
-const Article = () => {
+// 記事のページにリクエスト、アクセスする度にgetServerSideProps: GetServerSidePropsでレンダリングされ生成されている
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { slug } = context.params as params ;
+
+    // sampleCardsの配列からdataとゆう名前でひとつずつ取り出して、[slug](context)で渡ってきた値と一致するかをさ探してpageに格納
+    const page = sampleCards.find((data) => data.slug === slug);
+
+    return {
+        props: {
+            page: page ,
+        },
+    };
+};
+
+const Article: NextPage<ArticleProps> = ({ page }) => {
     return (
         <Layout>
         <article className="w-full">
             {/* meta section */}
             <div className="my-12">
-                <ArticleMeta />
+                <ArticleMeta page={page}/>
             </div>
     
             {/* article */}
-            <div className="my-12">article</div>
+            <div className="my-12">article { page.content }</div>
         </article>
         </Layout>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Card from '../components/Cards'
 import Layout from '../components/Layout'
 import Section from '../components/Section'
-import { siteConfig } from '../site.config'
+import { indexProps } from '../types/type'
+import { responsePage } from '../utils/notion'
 import { sampleCards } from '../utils/sample'
 
-const Home: NextPage = () => {
+// SSG = ビルドの段階でHTMLが生成される状態になっている = 事前生成 = ビルドが何度も行なわれるのがデメリット　= ビルドを行うと他のページも全てビルドし直すため処理が大変
+export const getStaticProps:GetStaticProps = async () => {
+  const { results } = await responsePage(); 
+  return {
+    props: {
+      pages: results ? results : [],
+    },
+    // ISR = SSGとSSRの中間のようなもの = ちょこちょこ更新するような場面において有効　= なぜなら秒数を指定した後、新しいHTMLを生成してくれて、次以降のリクエストに関してはそれを見せてくれる
+    revalidate: 10,
+  };
+};
+
+const Home: NextPage<indexProps> = ({ pages }) => {
+  console.log(pages);
   return (
     <Layout>
       <Section />
       <div className="pt-12">
-        <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
+        {/* <h1 className="text-5xl mb-8">{siteConfig.title}</h1> */}
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
           {sampleCards.map((page,index) => (
