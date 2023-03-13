18918c95e63026562dae0930df26374df19f68da
kameiryousuke <kamei.r@i.softbank.jp>
2023-03-13 10:36:48 +0900

    🔥

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Cursor, useTypewriter } from "react-simple-typewriter";
+// import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 const Section = () => {
-    const [text, count] = useTypewriter({
-        words: ["name is Kamei", "Loves-BullDog.tsx ", "<LovesToCodeMore/>"],
-        loop: true,
-        delaySpeed: 2000,
-    });
+    // const [text, count] = useTypewriter({
+    //     words: ["name is Kamei", "Loves-BullDog.tsx ", "<LovesToCodeMore/>"],
+    //     loop: true,
+    //     delaySpeed: 2000,
+    // });
 
     return (
         <div className="flex justify-between items-center bg-yellow-300 border-y border-black py-10 lg:py-0 max-w-6xl mx-auto">
@@ -14,7 +14,7 @@ const Section = () => {
             <h2 className="text-2xl sm:text-4xl font-serif max-w-xl">
             <span className="underline decoration-black decoration-3 font-serif">SunnyBlog</span> {""} is a place to write, read, and connect
             </h2>
-            <h3>//{text}</h3>
+            <h3>//</h3>
         </div>
 
         <h2 className="lg:text-[300px] md:text-[200px] text-[0px] mx-2 sm:mx-20 ">S</h2>
